4839e8f1e3ed000aef396d40505ef287ef52a5d8
Jesper Dangaard Brouer <hawk@comx.dk>
2011-05-05 16:58:56 -0400

    release.sh, should be in a working condition.
    Dropped the patch output.
    
    More verbose, on whats going on, while hiding some of the shell output.
    
    Signed-off-by: Jesper Dangaard Brouer <hawk@comx.dk>

--- release.sh
+++ release.sh
@@ -6,50 +6,57 @@
 #
 set -e
 NAME=iptv-analyzer
-VERSION=HEAD
+VERSION=0.7.1
 PREV_VERSION=0.7.0
+GPGKEY="D0777D99"
 
 echo "Creating tarball for release: $NAME-$VERSION"
-
-#VERSION_TAG="v{$VERSION}"
-VERSION_TAG=HEAD #HACK for testing
-
-GPGKEY="D0777D99"
+echo "============================="
 
 # Create a unique tempdir, to avoid leftovers from older release builds
 TMPDIR=`mktemp -dt $NAME.XXXXXXXXXX`
-#trap 'rm -rf $TMPDIR' EXIT
-echo TMPDIR:$TMPDIR
+trap 'rm -rf $TMPDIR' EXIT
+#echo TMPDIR:$TMPDIR
 PKGDIR="$TMPDIR/${NAME}-${VERSION}"
-echo PKGDIR:$PKGDIR
+#echo PKGDIR:$PKGDIR
 RELDIR=release
 if [ ! -d $RELDIR ]; then
     mkdir -p $RELDIR
 fi
+VERSION_TAG="v{$VERSION}"
+#VERSION_TAG=HEAD #HACK for testing
 
 # Compression packer tool
 packer=gzip
 packext=gz
 
-PATCH="$RELDIR/patch-$NAME-$PREV_VERSION-$VERSION.$packext";
+#PATCH="$RELDIR/patch-$NAME-$PREV_VERSION-$VERSION.$packext";
 TARBALL="$RELDIR/$NAME-$VERSION.tar.$packext";
-CHANGES="changes-$NAME-$PREV_VERSION-$VERSION.txt";
+CHANGES="$RELDIR/changes-$NAME-$PREV_VERSION-$VERSION.txt";
 
 #mkdir -p "$TMPDIR"
-git shortlog "v$PREV_VERSION..$VERSION_TAG" > "$RELDIR/$CHANGES"
-git diff "v$PREV_VERSION..$VERSION_TAG" | $packer > "$PATCH"
+echo " -- Git shortlog v$PREV_VERSION..$VERSION_TAG"
+git shortlog "v$PREV_VERSION..$VERSION_TAG" > "$CHANGES"
+#git diff "v$PREV_VERSION..$VERSION_TAG" | $packer > "$PATCH"
+echo " -- Git archiving version tag $VERSION_TAG"
 git archive --prefix="$NAME-$VERSION/" "$VERSION_TAG" | tar -xC "$TMPDIR/"
 
-pushd "$PKGDIR" && {
-	sh autogen.sh
-	popd
+pushd "$PKGDIR" > /dev/null && {
+    echo " -- Generating configure scripts..."
+    sh autogen.sh
+    popd > /dev/null
 }
 
+echo " -- Creating tarball $TARBALL"
 tar --use=${packer} -C "$TMPDIR" -cf "$TARBALL" "$NAME-$VERSION";
-gpg -u "$GPGKEY" -sb "$TARBALL";
+
+echo " -- Calculating checksums"
 md5sum "$TARBALL" >"${TARBALL}.md5sum";
 sha1sum "$TARBALL" >"${TARBALL}.sha1sum";
 
-gpg -u "$GPGKEY" -sb "$PATCH";
-md5sum "$PATCH" >"${PATCH}.md5sum";
-sha1sum "$PATCH" >"${PATCH}.sha1sum";
+echo " -- You need to sign the tarball"
+gpg -u "$GPGKEY" -sb "$TARBALL";
+
+#gpg -u "$GPGKEY" -sb "$PATCH";
+#md5sum "$PATCH" >"${PATCH}.md5sum";
+#sha1sum "$PATCH" >"${PATCH}.sha1sum";
