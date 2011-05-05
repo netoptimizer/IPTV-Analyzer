9b92e36f73ed48700c7aad9a6c89c9c2d04322cc
Jesper Dangaard Brouer <hawk@comx.dk>
2011-05-05 16:42:42 -0400

    release.sh, modified script for usage with iptv-analyzer.
    Script is in a testing phase.
    
    Signed-off-by: Jesper Dangaard Brouer <hawk@comx.dk>

--- release.sh
+++ release.sh
@@ -1,37 +1,55 @@
-#! /bin/sh
+#! /bin/bash
+#
+# Script for making releases, where you are forced to create a git tag
+# before you can make a release.  If you want tarballs without using a
+# git tag, then use the make tarball system.
 #
 set -e
-NAME=iptables
+NAME=iptv-analyzer
+VERSION=HEAD
+PREV_VERSION=0.7.0
 
-VERSION=1.4.7
-PREV_VERSION=1.4.6
+echo "Creating tarball for release: $NAME-$VERSION"
 
-GPGKEY="Netfilter Core Team"
+#VERSION_TAG="v{$VERSION}"
+VERSION_TAG=HEAD #HACK for testing
+
+GPGKEY="D0777D99"
 
 # Create a unique tempdir, to avoid leftovers from older release builds
 TMPDIR=`mktemp -dt $NAME.XXXXXXXXXX`
-trap 'rm -rf $TMPDIR' EXIT
+#trap 'rm -rf $TMPDIR' EXIT
+echo TMPDIR:$TMPDIR
 PKGDIR="$TMPDIR/${NAME}-${VERSION}"
+echo PKGDIR:$PKGDIR
+RELDIR=release
+if [ ! -d $RELDIR ]; then
+    mkdir -p $RELDIR
+fi
 
-PATCH="patch-$NAME-$PREV_VERSION-$VERSION.bz2";
-TARBALL="$NAME-$VERSION.tar.bz2";
-CHANGELOG="changes-$NAME-$PREV_VERSION-$VERSION.txt";
+# Compression packer tool
+packer=gzip
+packext=gz
+
+PATCH="$RELDIR/patch-$NAME-$PREV_VERSION-$VERSION.$packext";
+TARBALL="$RELDIR/$NAME-$VERSION.tar.$packext";
+CHANGES="changes-$NAME-$PREV_VERSION-$VERSION.txt";
 
 #mkdir -p "$TMPDIR"
-git shortlog "v$PREV_VERSION..v$VERSION" > "$TMPDIR/$CHANGELOG"
-git diff "v$PREV_VERSION..v$VERSION" | bzip2 > "$TMPDIR/$PATCH"
-git archive --prefix="$NAME-$VERSION/" "v$VERSION" | tar -xC "$TMPDIR/"
+git shortlog "v$PREV_VERSION..$VERSION_TAG" > "$RELDIR/$CHANGES"
+git diff "v$PREV_VERSION..$VERSION_TAG" | $packer > "$PATCH"
+git archive --prefix="$NAME-$VERSION/" "$VERSION_TAG" | tar -xC "$TMPDIR/"
 
 pushd "$PKGDIR" && {
 	sh autogen.sh
 	popd
 }
 
-tar -cjf "$TARBALL" "$NAME-$VERSION";
-gpg -u "$GPGKEY" -sb q"$TARBALL";
-md5sum "$TARBALL" >"$TARBALL.md5sum";
-sha1sum "$TARBALL" >"$TARBALL.sha1sum";
+tar --use=${packer} -C "$TMPDIR" -cf "$TARBALL" "$NAME-$VERSION";
+gpg -u "$GPGKEY" -sb "$TARBALL";
+md5sum "$TARBALL" >"${TARBALL}.md5sum";
+sha1sum "$TARBALL" >"${TARBALL}.sha1sum";
 
 gpg -u "$GPGKEY" -sb "$PATCH";
-md5sum "$PATCH" >"$PATCH.md5sum";
-sha1sum "$PATCH" >"$PATCH.sha1sum";
+md5sum "$PATCH" >"${PATCH}.md5sum";
+sha1sum "$PATCH" >"${PATCH}.sha1sum";
