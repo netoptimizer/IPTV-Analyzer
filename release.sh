39dd38a5210d05c3c81595a175e3fada25a1e184
Jesper Dangaard Brouer <hawk@comx.dk>
2011-05-05 15:52:59 -0400

    release.sh, use variable for package name.
    Signed-off-by: Jesper Dangaard Brouer <hawk@comx.dk>

--- release.sh
+++ release.sh
@@ -11,21 +11,21 @@ TMPDIR=`mktemp -dt $NAME.XXXXXXXXXX`
 trap 'rm -rf $TMPDIR' EXIT
 IPTDIR="$TMPDIR/${NAME}-${VERSION}"
 
-PATCH="patch-iptables-$PREV_VERSION-$VERSION.bz2";
-TARBALL="iptables-$VERSION.tar.bz2";
-CHANGELOG="changes-iptables-$PREV_VERSION-$VERSION.txt";
+PATCH="patch-$NAME-$PREV_VERSION-$VERSION.bz2";
+TARBALL="$NAME-$VERSION.tar.bz2";
+CHANGELOG="changes-$NAME-$PREV_VERSION-$VERSION.txt";
 
 #mkdir -p "$TMPDIR"
 git shortlog "v$PREV_VERSION..v$VERSION" > "$TMPDIR/$CHANGELOG"
 git diff "v$PREV_VERSION..v$VERSION" | bzip2 > "$TMPDIR/$PATCH"
-git archive --prefix="iptables-$VERSION/" "v$VERSION" | tar -xC "$TMPDIR/"
+git archive --prefix="$NAME-$VERSION/" "v$VERSION" | tar -xC "$TMPDIR/"
 
 cd "$IPTDIR" && {
 	sh autogen.sh
 	cd ..
 }
 
-tar -cjf "$TARBALL" "iptables-$VERSION";
+tar -cjf "$TARBALL" "$NAME-$VERSION";
 gpg -u "Netfilter Core Team" -sb "$TARBALL";
 md5sum "$TARBALL" >"$TARBALL.md5sum";
 sha1sum "$TARBALL" >"$TARBALL.sha1sum";
