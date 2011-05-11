c09892d9196522c161cd660127ad0c17e996b8c3
Jesper Dangaard Brouer <hawk@comx.dk>
2011-05-11 12:37:30 -0400

    iptables: extend mpeg2ts_stream with byte+packet counters, and cache align
    Extend struct mpeg2ts_stream in preparation for byte and packet
    counters per stream.
    
    Notice that the struct has been carefully cache aligned, to place
    highly modified members in the second cacheline.  Spacing in
    struct for cache alignment, is done via a ____cacheline_aligned
    trick, on "bytes", this makes "bytes" start on the next cacheline
    boundary, introducing a 16bytes hole.
    
    Signed-off-by: Jesper Dangaard Brouer <hawk@comx.dk>

--- iptables-module/xt_mpeg2ts.c
+++ iptables-module/xt_mpeg2ts.c
@@ -257,8 +257,19 @@ struct mpeg2ts_stream { /* Like xt_hashlimit: dsthash_ent */
 	struct hlist_node node;
 	struct mpeg2ts_stream_match match;
 
-	/* Place modified structure members in the end */
-	/* FIXME: Add spacing in struct for cache alignment */
+	/* PID list with last CC value (not updated too often) */
+	int pid_list_len;
+	struct list_head pid_list;
+
+	/* Place highly modified members after the next cache line */
+
+	/* Spacing in struct for cache alignment, is done via
+	   ____cacheline_aligned trick, on "bytes", this makes "bytes"
+	   start on the next cacheline boundary.  Introducing a
+	   (16bytes) hole here.
+	 */
+	uint64_t bytes ____cacheline_aligned;
+	uint64_t packets;
 
 	/* Per stream total skips and discontinuity */
 	/* TODO: Explain difference between skips and discontinuity */
@@ -271,12 +282,12 @@ struct mpeg2ts_stream { /* Like xt_hashlimit: dsthash_ent */
 	/* Usage counter to protect against dealloc/kfree */
 	atomic_t use;
 
-	/* PID list with last CC value */
-	struct list_head pid_list;
-	int pid_list_len;
-
 	/* For RCU-protected deletion */
 	struct rcu_head rcu_head;
+
+	/* Cacheline notes :*/
+	/* size: 128, cachelines: 2 */
+	/* sum members: 104, holes: 1, sum holes: 16 */
 };
 
 
