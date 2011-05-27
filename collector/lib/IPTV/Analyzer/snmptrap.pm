94bf805a34b9b1e95e20365e061ea2f1537928a3
Jesper Dangaard Brouer <hawk@comx.dk>
2011-05-26 23:46:16 -0400

    collector: Add very basic snmptrap helper module
    Signed-off-by: Jesper Dangaard Brouer <hawk@comx.dk>

---
+++ collector/lib/IPTV/Analyzer/snmptrap.pm
@@ -0,0 +1,162 @@
+package IPTV::Analyzer::snmptrap;
+
+=head1 NAME
+
+IPTV::Analyzer::snmptrap - module for sending SNMP traps events
+
+=head1 SYNOPSIS
+
+The IPTV::Analyzer::snmptrap module is a helper module for sending
+SNMP traps when events like no-signal occurs.
+
+=cut
+
+use strict;
+use warnings;
+
+use Net::SNMP qw(:ALL);
+
+#use Config::File;
+use Data::Dumper;
+
+use Log::Log4perl qw(get_logger :levels);
+our $logger = get_logger(__PACKAGE__);
+
+BEGIN {
+     use Exporter ();
+     our ($VERSION, @ISA, @EXPORT, @EXPORT_OK, %EXPORT_TAGS);
+
+     # Package version
+     $VERSION     = $IPTV::Analyzer::Version::VERSION;
+
+     @ISA         = qw(Exporter);
+     @EXPORT      = qw(
+                       open_snmp_session
+                       close_snmp_session
+                       send_snmptrap
+                      );
+
+}
+
+# Global vars
+our $snmp_session = undef;
+
+sub open_snmp_session($$)
+{
+    my $host      = shift;
+    my $community = shift;
+    my $version   = 2;
+
+    ($snmp_session, my $error) =
+	Net::SNMP->session(
+	    -hostname  => $host,
+	    -version   => $version,
+	    -community => $community,
+	    -port      => SNMP_TRAP_PORT
+	);
+
+    if (!defined($snmp_session)) {
+	$logger->fatal("Cannot start a SNMP session: $error");
+	return 0;
+	#exit 1
+    }
+    return 1;
+}
+
+sub close_snmp_session()
+{
+    $snmp_session->close();
+    $snmp_session = undef;
+}
+
+# taken from snmptrapd-sendtest.pl / Net-SNMPTrapd-0.04
+our %opt;
+$opt{version}   = $opt{version}   || 2;
+$opt{community} = $opt{community} || 'public';
+$opt{integer}   = $opt{integer}   || 1;
+$opt{string}    = $opt{string}    || 'String';
+$opt{oid}       = $opt{oid}       || '1.2.3.4.5.6.7.8.9';
+$opt{ip}        = $opt{ip}        || '10.10.10.1';
+$opt{counter32} = $opt{counter32} || 32323232;
+$opt{gauge32}   = $opt{gauge32}   || 42424242;
+$opt{timeticks} = $opt{timeticks} || time();
+$opt{opaque}    = $opt{opaque}    || 'opaque data';
+$opt{inform}    = $opt{inform}    || 0;
+
+
+sub send_snmptrap($$$$)
+{
+    my $event_type = shift || 0;
+    my $event_name = shift || "please-implement";
+    my $multicast  = shift;
+    my $src_ip     = shift;
+
+    if ( !defined($snmp_session) ) {
+	$logger->fatal("Cannot send SNMPtrap, no snmp session opened!");
+	return 0;
+    }
+
+    my $probe_ip   = "1.2.3.4"; # $cfg{'probe_ip'}
+    my $probe_name = "probe_name"; # $cfg{'probe_name'}
+
+    my $result = $snmp_session->snmpv2_trap(
+	-varbindlist  => [
+	     '1.3.6.1.2.1.1.3.0',         TIMETICKS,         time(),
+	     '1.3.6.1.6.3.1.1.4.1.0',     OBJECT_IDENTIFIER, '1.3.6.1.4.1.26124.43.2.2.1',
+
+	     # SNMPv2-MIB::sysLocation.0 ?
+	     #  snmptranslate -On SNMPv2-MIB::sysLocation.0
+	     #  .1.3.6.1.2.1.1.6.0
+
+	     # CollectorId
+	     '1.3.6.1.4.1.26124.43.1.1',  IPADDRESS,  $probe_ip,
+	     # CollectorName
+	     '1.3.6.1.4.1.26124.43.1.2',  OCTET_STRING,  $probe_name,
+
+	     # Event_type ID
+	     '1.3.6.1.4.1.26124.43.2.1',  INTEGER,           $event_type,
+	     # Event_type as text
+	     '1.3.6.1.4.1.26124.43.1.2',  OCTET_STRING,      $event_name,
+	     # Multicast dst IP
+	     '1.3.6.1.4.1.26124.2.1.3',  IPADDRESS,         $multicast,
+	     # Src IP
+	     '1.3.6.1.4.1.26124.2.1.4',  IPADDRESS,         $src_ip,
+
+	     # Also report the NIC interface?  IF-MIB::ifIndex
+	     #"1.3.6.1.2.1.2.2.1.1.$ifIndex", INTEGER,    $ifIndex,
+
+
+	     '1.3.6.1.4.1.26124.42.3.3',  INTEGER,           $opt{integer},
+	     '1.3.6.1.4.1.26124.42.3.4',  OCTET_STRING,      $opt{string},
+	     '1.3.6.1.4.1.26124.42.3.5',  OBJECT_IDENTIFIER, $opt{oid},
+	     '1.3.6.1.4.1.26124.42.3.6',  IPADDRESS,         $opt{ip},
+	     '1.3.6.1.4.1.26124.42.3.7',  COUNTER32,         $opt{counter32},
+	     '1.3.6.1.4.1.26124.42.3.8',  GAUGE32,           $opt{gauge32},
+	     '1.3.6.1.4.1.26124.42.3.9',  TIMETICKS,         $opt{timeticks},
+	     '1.3.6.1.4.1.26124.42.3.10', OPAQUE,            $opt{opaque}
+	  ]
+	)
+
+}
+
+1;
+__END__
+# Below is documentation for the module.
+#  One way of reading it: "perldoc IPTV/Analyzer/mpeg2ts.pm"
+
+=head1 DEPENDENCIES
+
+This module uses the module L<Net::SNMP> for sending snmptraps.
+
+=head1 AUTHOR
+
+Jesper Dangaard Brouer, E<lt>hawk@comx.dkE<gt> or E<lt>hawk@diku.dkE<gt>.
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2009-2011+ by Jesper Dangaard Brouer, ComX Networks A/S.
+
+This file is licensed under the terms of the GNU General Public
+License 2.0. or newer. See <http://www.gnu.org/licenses/gpl-2.0.html>.
+
+=cut
